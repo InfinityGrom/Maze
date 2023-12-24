f12659898aaf88cc24d594d52036cd791f057dac
AleGro <62838933+InfinityGrom@users.noreply.github.com>
2023-12-24 18:04:39 +0200

    Added movement and basic collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public GameObject Player;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position = new Vector2(transform.position.x + (h * speed / 100), transform.position.y + (v * speed / 100));
+
     }
 }
